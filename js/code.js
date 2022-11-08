2e9571e5c3028dbb839c10ae6e5599c51258d935
giftnkz6 <giftnkz6@gmail.com>
2022-11-08 10:12:21 +0200

    Added files

---
+++ js/code.js
@@ -0,0 +1,7 @@
+let firstName = prompt("Please enter your name");
+let lastName = prompt("Please enter your surname");
+let userSalary = prompt("Please enter your salary");
+let salarySum = eval(userSalary) + 500;
+console.log(`Name:${firstName}`);
+console.log(`Name:${lastName}`);
+console.log(`Name:${salarySum}`);
